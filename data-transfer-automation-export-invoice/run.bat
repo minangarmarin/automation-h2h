5a59736e4d3789eebcbc7cae145488adfc625863
minangarmarin <114040744+minangarmarin@users.noreply.github.com>
2024-02-06 23:14:53 +0700

    Delete data-transfer-automation-export-invoice directory

--- data-transfer-automation-export-invoice/run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"cmd" /K "cd C:\git\data-transfer-automation-export-invoice && .\gradlew clean test && .\gradlew runAfterTest"
-timeout /t 5 > nul
--- data-transfer-automation-export-invoice/run_headless_vm.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd "C:\git\data-transfer-automation-export-invoice"
-run_minimized.vbs run.bat
