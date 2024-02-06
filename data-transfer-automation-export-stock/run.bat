403c75b848a236c152181bb1e25c6b7d6451b26a
minangarmarin <114040744+minangarmarin@users.noreply.github.com>
2024-02-06 23:15:02 +0700

    Delete data-transfer-automation-export-stock directory

--- data-transfer-automation-export-stock/run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"cmd" /K "cd C:\git\data-transfer-automation-export-stock && .\gradlew clean test && .\gradlew runAfterTest"
-timeout /t 5 > nul
--- data-transfer-automation-export-stock/run_headless_vm.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd "C:\git\data-transfer-automation-export-stock"
-run_minimized.vbs run.bat
